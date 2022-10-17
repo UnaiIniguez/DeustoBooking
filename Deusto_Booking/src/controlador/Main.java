94dd82b3a0471098978810647ebf320491e3596e
Unai Iniguez <unai.iniguez@opendeusto.es>
2022-10-17 10:28:01 +0200

    Clase Main limpiada, y he creado una calse persona inicial

--- Deusto_Booking/src/controlador/Main.java
+++ Deusto_Booking/src/controlador/Main.java
@@ -4,9 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println( "Funciona" );
-		System.out.println( "Pruebo nuevo comit" );
-		System.out.println("Prueba Unai");
+		
 		
 	}
 	
---
+++ Deusto_Booking/src/modelo/Persona.java
@@ -0,0 +1,58 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable{
+	
+	private int dni;
+	private String nombre;
+	private String mail;
+	private int tlfNum; 
+	private int contrasenya;
+	
+	public Persona(int dni, String nombre, String mail, int tlfNum, int contrasenya) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.mail = mail;
+		this.tlfNum = tlfNum;
+		this.contrasenya = contrasenya;
+	}
+
+	public int getDni() {
+		return dni;
+	}
+
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public int getTlfNum() {
+		return tlfNum;
+	}
+
+	public void setTlfNum(int tlfNum) {
+		this.tlfNum = tlfNum;
+	}
+	
+	
+	
+	
+
+}
