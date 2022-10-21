ca501b6250891e278ca3c01065ff03f34f0642c6
Unai Iniguez <unai.iniguez@opendeusto.es>
2022-10-21 15:50:55 +0200

    Nuevas calses añadidas

---
+++ Deusto_Booking/src/modelo/Inmmueble.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Inmmueble {
+	
+	protected int numHab;
+	protected int numBany;
+	protected String ubicacion;
+	Propietario propietario;
+
+}
---
+++ Deusto_Booking/src/modelo/Propietario.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Propietario extends Persona{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Propietario(int dni, String nombre, String mail, int tlfNum, int contrasenya) {
+		super(dni, nombre, mail, tlfNum, contrasenya);
+		// TODO Auto-generated constructor stub
+	}
+
+}
