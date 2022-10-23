50f044e115c47f1e9939e2b845b91a7f96690626
Unai Iniguez <unai.iniguez@opendeusto.es>
2022-10-23 11:06:31 +0200

    Anyado clase cliente

---
+++ Deusto_Booking/src/modelo/Cliente.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class Cliente extends Persona{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	public Cliente(int dni, String nombre, String mail, int tlfNum, int contrasenya) {
+		super(dni, nombre, mail, tlfNum, contrasenya);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
