f3b7e0742823b59654df91bb817841e50712089a
UnaiIniguez <una.iniguez@opendeusto.es>
2022-10-31 10:03:20 +0100

    Formateo de la VentanaFormato

--- Deusto_Booking/src/GUI/VentanaFormato.java
+++ Deusto_Booking/src/GUI/VentanaFormato.java
@@ -8,32 +8,26 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class VentanaFormato extends JFrame{
-	
+public class VentanaFormato extends JFrame {
+
 	private JPanel j;
-	
-	public static final Color SKYBLUE = new Color(51,133,255);
-	
+
+	public static final Color SKYBLUE = new Color(51, 133, 255);
+
 	public VentanaFormato() {
-		
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setSize(500,500);
-	setTitle("Sign in");
-	setLocationRelativeTo(null);
-	getContentPane().setBackground(SKYBLUE);
-	//Icono de ventana
-	ImageIcon img = new ImageIcon("icono.png");
-	setIconImage(img.getImage());
-	
-	
-	
-	
-	
-	
-	
-	setVisible(true);
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(500, 500);
+		setTitle("Sign in");
+		setLocationRelativeTo(null);
+		getContentPane().setBackground(SKYBLUE);
+		// Icono de ventana
+		ImageIcon img = new ImageIcon("icono.png");
+		setIconImage(img.getImage());
+
+		setVisible(true);
 	}
-	
+
 	public static void main(String[] args) {
 		new VentanaFormato();
 	}
