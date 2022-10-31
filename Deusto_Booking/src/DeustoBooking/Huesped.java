73d30649a251d4b110ff42480c028a37d60b6dee
Unai Alonso <unai.alonso@opendeusto.es>
2022-10-31 13:20:23 +0100

    Modificación de atributos y constructor de Huesped

--- Deusto_Booking/src/DeustoBooking/Huesped.java
+++ Deusto_Booking/src/DeustoBooking/Huesped.java
@@ -2,13 +2,23 @@ package DeustoBooking;
 
 public class Huesped extends Persona {
 
-	private static final long serialVersionUID = 1L;
-	// Solvencia economica demostrable
-	private Boolean solvencia;
-
-	public Huesped(int dni, String nombre, String mail, int tlfNum, int contrasenya, Boolean solvencia) {
+	// Puesto de trabajo actual
+	private String cargo;
+	//Empresa en la que trabaja
+	private String nomEmpresa;
+	
+	public Huesped(int dni, String nombre, String mail, int tlfNum, int contrasenya, String cargo, String nomEmpresa) {
 		super(dni, nombre, mail, tlfNum, contrasenya);
-		this.solvencia = solvencia;
+		this.cargo = cargo;
+		this.nomEmpresa = nomEmpresa;
+		if (cargo == null) {
+			this.cargo = "Desempleado";
+			this.nomEmpresa = null;
+		}
 	}
+	
+	
+
+	
 
 }
