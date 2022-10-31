9d17706dd9955cfefbe502c8e7e26fa63b4ed0ec
Unai Alonso <unai.alonso@opendeusto.es>
2022-10-31 16:31:11 +0100

    Borrado de la clase Inquilino por redundancia

--- Deusto_Booking/src/modelo/Duenio.java
+++ Deusto_Booking/src/modelo/Duenio.java
@@ -1,9 +1,11 @@
 package modelo;
 
+import DeustoBooking.Persona;
+
 public class Duenio extends Persona  {
 	protected int numHab;
 	protected int numBany;
 	protected String ubicacion;
-	Propietario propietario;
+	Duenio propietario;
 
 }
