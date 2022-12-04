1a27334ef5d3819c18810c04c0aa4d3c5f53324b
UnaiIniguez <unai.iniguez@opendeusto.es>
2022-12-04 22:20:59 +0100

    Creada la conexion a la base de datos y el metodo de lectura de la base de datos

--- Deusto_Booking/src/controlador/Main.java
+++ Deusto_Booking/src/controlador/Main.java
@@ -1,8 +1,11 @@
 package controlador;
 
+import java.sql.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import deustoBooking.Gestor;
@@ -10,16 +13,56 @@ import gui.VentanaLogin;
 import gui.VentanaPrincipal;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		
 		
 		Gestor gestor = new Gestor();
 		VentanaPrincipal ventana = new VentanaPrincipal(gestor);
 		ventana.setVisible(true);
+		Gestor conexion = new Gestor();
+		Connection cn = conexion.conectar();
 		
+//========================Leer la Base de Datos===============================
+		
+		leerBaseDeDatos(cn);
 		
 	}
-	
+
+	// ========================Metodo para leer la Base de Datos======================================================================================================================================================
+
+	private static void leerBaseDeDatos(Connection cn) {
+
+		try {
+			Statement st = cn.createStatement();
+
+			// Para ver los datos de la BD se usa executeQuery; para borrar y actualizar
+			// datos se usa executeUpdate
+
+			ResultSet rs = st.executeQuery("SELECT * FROM sqlb_temp_table_1");
+
+			// Para iterar todos los datos
+			while (rs.next()) {
+
+				// Columnas
+				int precio = rs.getInt(1);
+				int max_hu = rs.getInt(2);
+				int ocupado = rs.getInt(3);
+				int num_hab = rs.getInt(4);
+				int num_bany = rs.getInt(5);
+				String ubi = rs.getString(6);
+				String tipo = rs.getString(7);
+				int m2 = rs.getInt(8);
+				int dni_d = rs.getInt(9);
+				// Lo muestro por pantalla
+				System.out.println(precio + " " + max_hu + " " + ocupado + " " + num_hab + " " + num_bany + " " + ubi + " " + tipo + " " + m2 + " " + dni_d);
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 
 }
--- Deusto_Booking/src/deustoBooking/Gestor.java
+++ Deusto_Booking/src/deustoBooking/Gestor.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.util.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,29 +15,27 @@ import java.util.Scanner;
 import javax.swing.text.html.HTMLDocument.Iterator;
 
 public class Gestor {
-	
+
 	private List<Duenio> propietarios = new ArrayList<>();
-	
-	private List<Huesped> huespedes = new ArrayList<>();		//Guardará a todos los huespedes de la base de datos
+
+	private List<Huesped> huespedes = new ArrayList<>(); // Guardará a todos los huespedes de la base de datos
 
 	private Map<String, ArrayList<Inmueble>> Reservas = new HashMap<>(); // En este mapa se almacenaran todos los
-																				// huespedes y los inmuebles que tiene
-																				// reservados.(Clave DNI)
+																			// huespedes y los inmuebles que tiene
+																			// reservados.(Clave DNI)
 
 	List<Inmueble> inmuebles = new ArrayList<>(); // Las viviendas que hay en la pagina web
-	
+
 	public void datosTest() {
-		
+
 		ArrayList<Inmueble> inmueblesTest = new ArrayList<>();
 		Duenio d = new Duenio("12345678B", "Pepe", 77, "pepe@gmail.com", "656232359", "1234", inmueblesTest);
 		propietarios.add(d);
-		
+
 	}
 
-	
-
 	public Gestor() {
-		
+
 	}
 
 	public List<Duenio> getPropietarios() {
@@ -51,15 +50,11 @@ public class Gestor {
 		return Reservas;
 	}
 
-
 	public List<Inmueble> getInmuebles() {
 		return inmuebles;
 	}
 
-
 //**************************METODOS COMUNES**********************************************
-	
-
 
 	/**
 	 * 
@@ -86,82 +81,76 @@ public class Gestor {
 		return false;
 		
 	}
-	
-	
-	
+
 //********************METODOS DEL ANFITRIÓN********************************
-	
-	
-	
+
 	/**
 	 * 
-	 * Anyade un inmueble a la web 
+	 * Anyade un inmueble a la web
 	 * 
-	 * @param Duenio = El duenio que quiere anyadir el inmueble
-	 *  @param Inmueble = inmueble que quiere anyadir
+	 * @param Duenio   = El duenio que quiere anyadir el inmueble
+	 * @param Inmueble = inmueble que quiere anyadir
 	 *
 	 */
-	public void anadirInmueble( Duenio duenio, Inmueble inmueble ) {
-		
+	public void anadirInmueble(Duenio duenio, Inmueble inmueble) {
+
 		duenio.getInmuebles().add(inmueble);
 		inmuebles.add(inmueble);
-		
+
 	}
-	
+
 	/**
 	 * 
-	 * Borra un inmueble de la web 
+	 * Borra un inmueble de la web
 	 * 
-	 * @param Duenio = El duenio que quiere eliminar el inmueble
-	 *  @param Inmueble = inmueble que quiere eliminar
+	 * @param Duenio   = El duenio que quiere eliminar el inmueble
+	 * @param Inmueble = inmueble que quiere eliminar
 	 *
 	 */
-	public void borrarInmueble( Duenio duenio, Inmueble inmueble) {
-		
+	public void borrarInmueble(Duenio duenio, Inmueble inmueble) {
+
 		duenio.getInmuebles().remove(inmueble);
 		inmuebles.remove(inmueble);
-		
+
 	}
-	
-	
+
 	/**
 	 * 
 	 * Editar el inmueble (Cambios en el espacio, mas habitaciones...)
 	 * 
-	 * @param Duenio = El duenio que quiere editar el inmueble
+	 * @param Duenio   = El duenio que quiere editar el inmueble
 	 * @param Inmueble viejo = El viejo inmueble que se quiere editar
-	 * @param Inmueble nuevo = El inmueble con los cambios   
+	 * @param Inmueble nuevo = El inmueble con los cambios
 	 *
 	 */
 	public void editarInmueble(Duenio duenio, Inmueble inmuebleviejo, Inmueble nuevoInmueble) {
-		
+
 		duenio.getInmuebles().remove(inmuebleviejo);
 		inmuebles.remove(inmuebleviejo);
-		
+
 		duenio.getInmuebles().add(nuevoInmueble);
 		inmuebles.add(nuevoInmueble);
-		
+
 	}
-	
-	
-	//********************METODOS DEL HUESPED********************************
-	
+
+	// ********************METODOS DEL HUESPED********************************
+
 	/**
 	 * 
-	 * Anular reserva 
+	 * Anular reserva
 	 * 
 	 * @param h = Huesped que anula la reserva
 	 * @param i = Inmueble reservado pendiente de anular
 	 *
 	 */
 	public void anularReserva(Huesped h, Inmueble i) {
-		if ( Reservas.containsKey(h.getDni()) ) {
-			if(Reservas.get(h.getDni()).contains(i)) {
+		if (Reservas.containsKey(h.getDni())) {
+			if (Reservas.get(h.getDni()).contains(i)) {
 				Reservas.get(h.getDni()).remove(i);
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 * Reservar
@@ -172,65 +161,82 @@ public class Gestor {
 	 */
 	public void reservar(Huesped h, Inmueble i) {
 		if (Reservas.containsKey(h.getDni())) {
-			if(!Reservas.get(h.getDni()).contains(i)) {
+			if (!Reservas.get(h.getDni()).contains(i)) {
 				Reservas.get(h.getDni()).add(i);
 			}
-		}else {
+		} else {
 			Reservas.put(h.getDni(), new ArrayList<Inmueble>());
 			Reservas.get(h.getDni()).add(i);
 		}
 	}
-	
+
 	public boolean iniciarSesionDB(String dni, String contrasenya) {
-		
+
 		Connection conn = null;
-		{try {
-			
-			Class.forName("org.sqlite.JDBC");
-			conn = DriverManager.getConnection("jdbc:sqlite:db/Deusto_Booking.db");
-			System.out.println("Abre la DB");
-			
-			Statement stmt = conn.createStatement();
-			
-			List<String> usuarios = new ArrayList<>();
-			ResultSet rs1 = stmt.executeQuery(" SELECT DNI_H FROM Huesped");
-			while(rs1.next()) {
-				usuarios.add(rs1.getString("DNI_H"));
+		{
+			try {
+
+				Class.forName("org.sqlite.JDBC");
+				conn = DriverManager.getConnection("jdbc:sqlite:db/Deusto_Booking.db");
+				System.out.println("Abre la DB");
+
+				Statement stmt = conn.createStatement();
+
+				List<String> usuarios = new ArrayList<>();
+				ResultSet rs1 = stmt.executeQuery(" SELECT DNI_H FROM Huesped");
+				while (rs1.next()) {
+					usuarios.add(rs1.getString("DNI_H"));
+				}
+
+				List<String> contrasenyas = new ArrayList<>();
+				ResultSet rs2 = stmt.executeQuery(" SELECT Contrasenya_H FROM Huesped");
+				while (rs2.next()) {
+					usuarios.add(rs2.getString("Contrasenya_H"));
+				}
+
+				if (usuarios.contains(dni) && contrasenyas.contains(contrasenya)
+						&& usuarios.indexOf(dni) == contrasenyas.indexOf(contrasenya)) {
+					return true;
+				}
+
+				stmt.close();
+				conn.close();
+			} catch (Exception e) {
+				System.out.println("Error de conexion a la BD");
 			}
-			
-			List<String> contrasenyas = new ArrayList<>();
-			ResultSet rs2 = stmt.executeQuery(" SELECT Contrasenya_H FROM Huesped");
-			while(rs2.next()) {
-				usuarios.add(rs2.getString("Contrasenya_H"));
-			}
-			
-			if(usuarios.contains(dni) && contrasenyas.contains(contrasenya) && usuarios.indexOf(dni) == contrasenyas.indexOf(contrasenya)) {
-				return true;
-			}
-			
-			
-			stmt.close();
-			conn.close();
-		}catch(Exception e) {
-			System.out.println("Error de conexion a la BD");
-		}
-			
+
 		}
 		return false;
-	
 
 	}
-	
-	public void registroHuesped(String dni, String nombre, int edad, String mail, String tlf, String cargo, String nomEmpresa, String contrasenya) {
-		
+
+	public void registroHuesped(String dni, String nombre, int edad, String mail, String tlf, String cargo,
+			String nomEmpresa, String contrasenya) {
+
 		Huesped h = new Huesped(dni, nombre, edad, mail, tlf, contrasenya, cargo, nomEmpresa);
-		
+
 		if (!huespedes.contains(h)) {
 			huespedes.add(h);
 		}
 	}
-	
-	
+
+	// ========================Metodo para conenctarme a la Base de Datos======================================================================================================================================================
+
+	private Connection conectar;
+
+	public Connection conectar() {
+		try {
+			Class.forName("org.mysql.JDBC");
+
+			conectar = DriverManager.getConnection("jdbc:mysql:Deusto_Booking.db");
+			System.out.println("Conexion establecida");
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return conectar;
+
+	}
+
+
 }
-
-
