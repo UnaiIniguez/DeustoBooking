9455bb80c69413aee4439fe46890db6c48a24c9e
Unai Alonso <unai.alonso@opendeusto.es>
2022-12-28 20:18:12 +0100

    Añadida tabla Reserva a bd y actualizacion de atributos en duenyo y huesped

--- Deusto_Booking/utilidades/Cifrar.java
+++ Deusto_Booking/utilidades/Cifrar.java
@@ -8,18 +8,23 @@ import java.util.Base64;
 public class Cifrar {
 
 	
-	public static String cifrar (String s) throws NoSuchAlgorithmException {
+	public static String cifrar (String s) {
 		
-		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-	    byte[] byteParaCifrar = s.getBytes(StandardCharsets.UTF_8);
-	    byte[] byteArrayCifrado = digest.digest(byteParaCifrar);
-	    String cifrado = Base64.getEncoder().encodeToString(byteArrayCifrado);
-	    return cifrado;
-		
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] byteParaCifrar = s.getBytes(StandardCharsets.UTF_8);
+			byte[] byteArrayCifrado = digest.digest(byteParaCifrar);
+			String cifrado = Base64.getEncoder().encodeToString(byteArrayCifrado);
+			return cifrado;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	
-	public static void main(String []args) throws NoSuchAlgorithmException {
+	public static void main(String []args) {
 		
 		System.out.println(cifrar("Ivan"));
 	}
