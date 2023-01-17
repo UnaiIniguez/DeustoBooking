2ec7a307eb46e6dcf5d0fd5541893db6a34ed49a
inigoohernaezz <inigo.hernaez@opendeusto.es>
2023-01-17 11:24:57 +0100

    Completar HuespedTest que faltaban algunas cosas

--- Deusto_Booking/Test/HuespedTest.java
+++ Deusto_Booking/Test/HuespedTest.java
@@ -46,5 +46,88 @@ public class HuespedTest {
 		assertEquals ("Deusto", huesped.getNomEmpresa());
 	}
 	
+	@Test
+	public void getDniTest() {
+		assertEquals("11111111J", huesped.getDni());
+	}
+	
+	@Test
+	public void setDniTest() {
+		huesped.setDni("58050911W");
+		assertEquals("58050911W", huesped.getDni());
+	}
+	
+	@Test
+	public void getNombreTest() {
+		
+		assertEquals("Andres", huesped.getNombre());
+	}
+	
+	@Test
+	public void setNombreTest() {
+		huesped.setNombre("Javier");
+		assertEquals("Javier", huesped.getNombre());
+	}
+	
+	@Test
+	public void getMailTest() {
+		assertEquals("andres@opendeusto.es", huesped.getMail());
+	}
+	
+	@Test
+	public void setMailTest() {
+		
+		huesped.setMail("javier@gmail.com");
+		assertEquals("javier@gmail.com", huesped.getMail());
+	}
+	
+	@Test
+	public void getTlfNumTest() {
+		
+		assertEquals("607343434", huesped.getTlfNum());
+	}
+	
+	@Test
+	public void setTlfNumTest() {
+		huesped.setTlfNum("636480492");
+		assertEquals("636480492", huesped.getTlfNum());
+	
+	}
+	
+	@Test
+	public void getEdadTest() {
+		
+		assertEquals(32, huesped.getEdad());
+		
+	}
+	
+	@Test
+	public void setEdadTest() {
+		
+		huesped.setEdad(33);
+		assertEquals(33, huesped.getEdad());
+		
+	}
+	
+	@Test
+	public void getContrasenyaTest() {
+		assertEquals("Deusto24", huesped.getContrasenya());
+	}
+	
+	@Test
+	public void setContrasenyaTest() {
+		
+		huesped.setContrasenya("11111");
+		assertEquals("11111", huesped.getContrasenya());
+	}
+	
+	@Test
+	public void equalsTest() {
+		Huesped p = new Huesped("48930253Q", "Javier", 22, "javier@gmail.com", "252637282", "11111", "Informatico", "Microsoft");
+		assertEquals(false, huesped.equals(p));
+	
+	}
+	
+	
 	
 }
